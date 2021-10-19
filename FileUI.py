ec0d5d799f0b0ef5f4d504d9b8c7982088fbe4d6
Dan Hayes <4745454B@Rutabaga.tech>
2021-10-19 16:11:22 -0400

    Developing path and filename input and storage

--- FileUI.py
+++ FileUI.py
@@ -2,6 +2,7 @@
 Develop user interface for accessing files in fhx_processing.py.
 """
 
+import os
 
 def func_display_menu():
     """ Displays an option menu """
@@ -12,32 +13,50 @@ def func_display_menu():
     print("3  Execute fhx_processing")
     print()
     print("Select option and press Enter")
-    response = input("Press enter without an option to exit ")
+    response = int(input("Press enter without an option to exit "))
     print("response = ", response)
 
-    if response == str(1):
+    if response == 1:
         func_get_path()
-    elif response == str(2):
+    elif response == 2:
         func_get_filename()
     else:
         pass
 
 def func_get_path():
-    print("Get Path")
+    #Prompt for path
+    #Consider example path for different operating systems
+    path_in = input("Enter the path to the .fhx file (C:\\Folder\\) ")
+    print(path_in)
+
+    #Verify path
+
+    #Implement error handling
+
+    #Store path
     func_store_path()
 
 def func_store_path():
-    print("Store Path")
+    filename = "var_store/path_var.txt"
+    os.makedirs(os.path.dirname(filename), exist_ok=True)
+    with open(filename, 'w', newline='') as path_var_stor:
+        path_var_stor.write("C:\Test\Path\\")
+    print("Path Stored")
 
 def func_get_filename():
-    print("Get Filename")
+    filename_in = input("Enter the .fhx file filename: ")
+    print(filename_in)
     func_store_filename()
 
 def func_store_filename():
-    print("Store Filename")
+    filename = "var_store/filename_var.txt"
+    os.makedirs(os.path.dirname(filename), exist_ok=True)
+    with open(filename, 'w', newline='') as filename_var_stor:
+        filename_var_stor.write("Filename")
+    print("Filename Stored")
 
 def func_display_current():
-    pass
+    print("Display Current File Properties")
 
 def main():
     # Display option menu
